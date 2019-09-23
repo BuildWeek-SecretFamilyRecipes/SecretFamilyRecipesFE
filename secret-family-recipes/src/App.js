c91b7d8ceced88ab230637b379f48ccd62729ee5
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-09-23 15:13:49 -0400

    navbar to link to sign up page

--- secret-family-recipes/src/App.js
+++ secret-family-recipes/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import LoginForm from "./components/LoginForm"
+import LoginForm from "./components/LoginForm";
 import './App.css';
+import LoginNav from "./components/LoginNav";
 
 function App() {
   return (
     <div className="App">
+      <LoginNav />
       <LoginForm /> 
     </div>
   );
---
+++ secret-family-recipes/src/components/LoginNav.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+
+function LoginNav() {
+    return (
+      <div className="nav-bar"> 
+        <nav>
+            <h1>Secret Family Recipes</h1>
+            <a href="#">Sign Up</a>
+        </nav>
+      </div>
+    );
+}
+
+export default LoginNav;
