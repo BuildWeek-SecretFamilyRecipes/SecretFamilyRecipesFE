e504fdcc0ae19052a4ffa791d850aca12cc42328
Tyler Biswell <48727035+TylerBiswell@users.noreply.github.com>
2019-09-24 11:17:08 -0600

    Revert "Tyler biswell"

--- secret-family-recipes/src/index.js
+++ secret-family-recipes/src/index.js
@@ -1,23 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import thunk from 'redux-thunk'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import rootReducer from './Reducers'
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(
-	<Provider store={store}>
-		<Router>
-			<App />
-		</Router>
-	</Provider>,
-	document.getElementById('root')
-)
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
+
 
 
 
