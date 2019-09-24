40a75ed3d4ef2e9a1d2c93364c32eb4fd3fb477b
Tyler Biswell <tsbiswell@gmail.com>
2019-09-24 00:21:40 -0600

    Built an entire new webpage that has no real functionality but it helped me get over breaking everything

--- secret-family-recipes/src/App.js
+++ secret-family-recipes/src/App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import LoginForm from "./components/LoginForm";
-import './App.css';
-import LoginNav from "./components/LoginNav";
+// import React from 'react';
+// import LoginForm from "./components/LoginForm";
+// import './App.css';
+// import LoginNav from "./components/LoginNav";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <LoginNav />
+//       <LoginForm /> 
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react'
+// import './App.css'
+import HomePage from './components/HomePage/HomePage'
 
 function App() {
   return (
     <div className="App">
-      <LoginNav />
-      <LoginForm /> 
+      <HomePage />
     </div>
   );
 }
